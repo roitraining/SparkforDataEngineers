5e380b097421b62902a71fdfaf1c604c3e0b3f8e
Joseph Gagliardo <joey@me.com>
2019-10-10 14:58:01 -0400

    0.1.02

--- Ch01_Lab.py
+++ Ch01_Lab.py
@@ -11,11 +11,12 @@ cc = cc.filter(lambda x : x != first)
 cc.take(10)
 cc = cc.map(lambda x : x.split(',')) 
 cc.take(10)
-cc = cc.map(lambda x : ((x[0][1:], x[1][1:-1]), (x[5], float(x[6]))))
+cc = cc.map(lambda x : ((x[0][1:], x[1][1:-1], x[5], float(x[6]))))
 #print (cc.collect())
 print(cc.take(10))
 
 # Bonus
+cc = cc.map(lambda x : ((x[0][1:], x[1][1:-1]), (x[5], float(x[6]))))
 ccf = cc.filter(lambda x : x[1][0] == 'F').map(lambda x : (x[0], x[1][1]))
 ccg = ccf.reduceByKey(lambda x, y : x + y)
 #print (ccg.sortByKey().collect())
